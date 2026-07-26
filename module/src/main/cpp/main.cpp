273013096c5caa3e77ec8a91b9dbb82102c51959
wanglingDev <enriqoumar@gmail.com>
2026-07-26 15:19:51 +0800

    11

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,38 +1,16 @@
-#include <cstring>
-#include <jni.h>
 #include <pthread.h>
+#include <cstring>
 #include "hook.h"
-#include "zygisk.hpp"
 
-using zygisk::Api;
-using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
-class MyModule : public zygisk::ModuleBase {
-public:
-    void onLoad(Api *api, JNIEnv *env) override {
-        env_ = env;
+// Entry point untuk external injector (Android-GUI-Injector, AndKittyInjector, dll)
+// Dipanggil otomatis saat .so di-dlopen ke dalam proses game
+__attribute__((constructor)) void lib_main() {
+    pthread_t ntid;
+    int ret = pthread_create(&ntid, nullptr, hack_thread, nullptr);
+    if (ret != 0) {
+        LOGE("lib_main: can't create hack_thread: %s", strerror(ret));
+    } else {
+        LOGI("lib_main: hack_thread spawned");
+        pthread_detach(ntid);
     }
-
-    void preAppSpecialize(AppSpecializeArgs *args) override {
-        if (!args || !args->nice_name) {
-            LOGE("Skip unknown process");
-            return;
-        }
-        enable_hack = isGame(env_, args->app_data_dir);
-    }
-
-    void postAppSpecialize(const AppSpecializeArgs *) override {
-        if (enable_hack) {
-            int ret;
-            pthread_t ntid;
-            if ((ret = pthread_create(&ntid, nullptr, hack_thread, nullptr))) {
-                LOGE("can't create thread: %s\n", strerror(ret));
-            }
-        }
-    }
-
-private:
-    JNIEnv *env_{};
-};
-
-REGISTER_ZYGISK_MODULE(MyModule)
+}
